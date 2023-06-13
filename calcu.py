7a089162ed6d2ca391a52a178bd9391891e71c03
aisyakmila <130299353+aisyakmila@users.noreply.github.com>
2023-06-13 19:22:49 +0700

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-from scipy import stats
 
 test = st.sidebar.selectbox("Distribusi Diskrit", ['Home', 'Binomial', 'Hipergeometrik', 'Geometrik', 'Binomial Negatif','Poisson'])
 
